bcf51359a026ab873a10f808b0b3c253db5db00d
nmarum <nick.marum@gmail.com>
2020-09-30 20:48:38 -0400

    Revision to RMD and final R script

--- RMSE_final_model.R
+++ RMSE_final_model.R
@@ -1,31 +1,24 @@
 # Demonstration of final model and RMSE
-
-library(recommenderlab)
+if(!require(recosystem)) install.packages("recosystem")
 library(tidyverse)
 library(recosystem)
-load("edx.rda")
-load("validation.rda")
+
+load("edx.rda")#loading edx object from working directory
+load("validation.rda")#loading validation hold-out object from working directory
+
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
-}
+} #RMSE function as defined in text book
 
-mu <- mean(edx$rating)
+set.seed(1, sample.kind="Rounding") #ensure consistency of results through set seed.
 
-res <- edx %>% 
-  group_by(userId) %>%
-  mutate(bu = mean(rating-mu)) %>% #user effect is adjusted through normalization
-  ungroup() %>%
-  group_by(movieId) %>%
-  mutate(bi = sum(rating-mu)/(n()+.5)) %>% #movie effect is adjusted through regularization
-  ungroup() %>%
-  mutate(res = rating-bu-bi)#residuals found through removing user and movie effects
-
-train <- data_memory(res$userId, res$movieId, rating = res$rating) 
+train <- data_memory(edx$userId, edx$movieId, rating = edx$rating) 
 #sparse matrix of residuals are basis for training matrix factorization model
 
 r = Reco() #creating the Recommender System Object of the recosys package
 
-r$train(train_data = train, opts = list(costq_l1 = 0))
+r$train(train_data = train, opts = list(dim=30, lrate = .1, costp_l1 = 0,
+                                        costp_l2=.1, costq_l1=0, costq_l2=.1))
 #LIMBF model is trained using residual sparse matrix
 
 valid <- data_memory(validation$userId, validation$movieId) 
@@ -35,12 +28,13 @@ valid <- data_memory(validation$userId, validation$movieId)
 
 r$predict(test_data = valid)
 #prediction function for recosys based on validation data - it 
-#produces a file in the working directory that contains predictions. This can take a while.
+#produces a file in the working directory that contains predictions. 
+#This can take a while.
 
 pred <-read.csv("predict.txt", header = FALSE)
 #reading the prediction file into the local environment.  Predictions are in CSV
 #format without a header.
 
 RMSE(validation$rating, pred$V1)
-#.8324285 RMSE
+#.819 RMSE
 
